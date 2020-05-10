ff74c1fdc5df63acd39f8cab85a217c1839b0e13
IronMan26709 <49660049+IronMan26709@users.noreply.github.com>
2020-05-10 21:26:07 +0300

    fix: dont start docker, wip [ref:no-ref]

--- dist/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from 'mongoose';
-export declare class UsersService {
-    private readonly userModel;
-    constructor(userModel: Model<any>);
-    findUser(query: string): Promise<any>;
-    find(query: any): Promise<any>;
-}
