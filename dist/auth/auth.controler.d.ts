cdc36931536664e286387366d363bcf997b521a9
Andrew <58174846+IronMan756@users.noreply.github.com>
2020-05-25 00:45:14 +0300

    Delete auth.controler.d.ts

--- dist/auth/auth.controler.d.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from "express";
-export declare class AuthController {
-    constructor();
-    signin(query: any, res: Response): Promise<Response<any>>;
-    signUp(user: any, res: Response): Promise<Response>;
-}
