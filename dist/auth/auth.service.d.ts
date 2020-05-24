e040d5b391ce43041e491575d6eca9748730f1c4
Andrew <58174846+IronMan756@users.noreply.github.com>
2020-05-25 00:44:30 +0300

    Delete auth.service.d.ts

--- dist/auth/auth.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { JwtService } from "@nestjs/jwt";
-export declare class AuthService {
-    private readonly jwtSerwice;
-    constructor(jwtSerwice: JwtService);
-    createJwt(user: any): Promise<string>;
-}
