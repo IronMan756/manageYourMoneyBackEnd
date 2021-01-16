173bd4a573210f5484d8de26862f1ead45befa20
IronMan756 <andrewstarudubtsev@gmail.com>
2021-01-17 01:58:19 +0200

    feat: create expence[ref:-no-ref]

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,29 +4,26 @@ import { Model } from 'mongoose';
 import { UserDto } from "./users.dto";
  
 @Injectable()
-export class UsersService{
-    public constructor(
-        @InjectModel('User') private readonly userModel: Model<any>
-    ){}
-    public async findUser(query: any): Promise<any>{
-        return this.userModel
-            .findOne(query)
-            .lean()
-            .exec();
-
-    }
-    public async find(): Promise<any>{
-        return this.userModel
-            .find({})
-            .lean()
-            .exec();
-
-    }
-    public async createUser (
-        user: UserDto & { accessToken: string}
-    ):Promise<UserDto>{
-        console.log(user)
-        const createUser = new this.userModel(user);
-        return createUser.save();
-    }
+export class UsersService {
+  public constructor(
+    @InjectModel("User") private readonly userModel: Model<any>
+  ) {}
+  public async findUser(email: string): Promise<any> {
+    return this.userModel
+      .findOne({ email }, { _id: 0, __v: 0 })
+      .lean()
+      .exec();
+  }
+  public async find(): Promise<any> {
+    return this.userModel
+      .find({})
+      .lean()
+      .exec();
+  }
+  public async createUser(
+    user: UserDto & { accessToken: string }
+  ): Promise<UserDto> {
+    const createUser = new this.userModel(user);
+    return createUser.save();
+  }
 }
