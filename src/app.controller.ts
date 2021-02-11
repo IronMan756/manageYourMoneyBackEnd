b5a387868b2e70e04fe17ad21f938b55e40d7bf5
IronMan756 <andrewstarudubtsev@gmail.com>
2021-02-11 19:55:12 +0200

    feat: deploy to AWS try enother way[ref:no-ref]

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpStatus, Res } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { Response } from "express";
 
 @Controller()
 export class AppController {
-  
   constructor() {}
+  @Get("")
+  @ApiOperation({ description: "Find user" })
+  @ApiResponse({
+    description: "Find user success",
+    status: HttpStatus.OK,
+  })
+  @ApiResponse({
+    description: "Any user weren't found",
+    status: HttpStatus.NOT_FOUND,
+  })
+  @ApiResponse({
+    description: "Server error",
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+  })
+  public async findUser(@Res() res: Response) {
+    try {
+      return res.status(HttpStatus.OK).json({
+        // data: [user],
+        data: "It is succceeesss!!!! Ura!!!!!",
+        error: null,
+      });
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ data: null, error });
+    }
+  }
 }
