328206df28129ae7c6de1975bfcb44abeb82e9ec
Andrew <58174846+IronMan756@users.noreply.github.com>
2021-02-11 19:42:13 +0200

    Delete dist directory

--- dist/users/users.constroller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from "./users.service";
-import { Response } from "express";
-export declare class UsersController {
-    private readonly _userServise;
-    constructor(_userServise: UsersService);
-    findUser(quary: any, res: Response): Promise<Response<any>>;
-}
